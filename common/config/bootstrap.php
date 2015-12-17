8fdf68ebb16b353cc8f142ee0f878cf2d9eb73aa
wciq1208 <21181612@qq.com>
2015-12-17 22:58:00 +0800

    update

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,7 +4,6 @@ Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
 
-session_set_save_handler(new common\SessionHandler);
 yii\base\Event::on("yii\web\User", yii\web\User::EVENT_AFTER_LOGIN, function($event){
     Yii::$app->session->set('username', $event->identity->username);
 });
--- common/config/main.php
+++ common/config/main.php
@@ -3,7 +3,8 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'session' => [
-            'class' => 'yii\web\CacheSession'
+            'class' => 'yii\web\CacheSession',
+            'handler' => 'common\SessionHandler',
         ],
     ],
 ];
