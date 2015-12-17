89f828bafb5d13b144c5c85b53d60e0e6f57ee89
wciq1208 <21181612@qq.com>
2015-12-17 22:02:57 +0800

    用户登录后SESSION记录用户名,改变session存储格式

---
+++ common/SessionHandler.php
@@ -0,0 +1,66 @@
+<?php
+namespace common;
+
+/**
+* 
+*/
+class SessionHandler extends \SessionHandler implements \SessionHandlerInterface
+{
+    public function write($id, $data){
+        var_dump(json_encode(self::unserialize($data)));
+        return parent::write($id, json_encode(self::unserialize($data)));
+    }
+
+    public function read($id){
+        var_dump(json_decode(parent::read($id),true));
+        return json_decode(parent::read($id), true);
+    }
+
+    private static function unserialize_php($session_data) {
+        $return_data = array();
+        $offset = 0;
+        while ($offset < strlen($session_data)) {
+            if (!strstr(substr($session_data, $offset), "|")) {
+                throw new Exception("invalid data, remaining: " . substr($session_data, $offset));
+            }
+            $pos = strpos($session_data, "|", $offset);
+            $num = $pos - $offset;
+            $varname = substr($session_data, $offset, $num);
+            $offset += $num + 1;
+            $data = unserialize(substr($session_data, $offset));
+            $return_data[$varname] = $data;
+            $offset += strlen(serialize($data));
+        }
+        return $return_data;
+    }
+
+    public static function unserialize($session_data) {
+        $method = ini_get("session.serialize_handler");
+        switch ($method) {
+            case "php":
+                return self::unserialize_php($session_data);
+                break;
+            case "php_binary":
+                return self::unserialize_phpbinary($session_data);
+                break;
+            default:
+                throw new Exception("Unsupported session.serialize_handler: " . $method . ". Supported: php, php_binary");
+        }
+    }
+
+    private static function unserialize_phpbinary($session_data) {
+        $return_data = array();
+        $offset = 0;
+        while ($offset < strlen($session_data)) {
+            $num = ord($session_data[$offset]);
+            $offset += 1;
+            $varname = substr($session_data, $offset, $num);
+            $offset += $num;
+            $data = unserialize(substr($session_data, $offset));
+            $return_data[$varname] = $data;
+            $offset += strlen(serialize($data));
+        }
+        return $return_data;
+    }
+
+}
--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,3 +3,11 @@ Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
+
+session_set_save_handler(new common\SessionHandler);
+yii\base\Event::on("yii\web\User", yii\web\User::EVENT_AFTER_LOGIN, function($event){
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
+    $_SESSION['username'] = $event->identity->username;
+});
