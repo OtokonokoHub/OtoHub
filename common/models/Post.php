bb8c9847490b63788ef106aee84f5a9223ea2bb2
wciq1208 <21181612@qq.com>
2015-12-22 10:03:19 +0800

    test sock

--- common/models/Post.php
+++ common/models/Post.php
@@ -62,7 +62,7 @@ class Post extends \yii\db\ActiveRecord
     public function insert($runValidation = true, $attributes = NULL){
         $result = parent::insert($runValidation, $attributes);
         if ($result) {
-            $f = fsockopen('tcp://127.0.0.1', 5555);
+            $f = fsockopen('/dev/shm/feed.sock');
             if ($f) {
                 fwrite($f, json_encode(['post_id' => $this->id, 'user_id' => Yii::$app->user->getId()]));
             }
