86252eeb0c3b3c56467fb41be473ca727556ef0e
wciq1208 <21181612@qq.com>
2015-12-23 15:07:35 +0800

    update

--- frontend/components/PostBehavior.php
+++ frontend/components/PostBehavior.php
@@ -20,7 +20,7 @@ class PostBehavior extends Behavior
             ActiveRecord::EVENT_AFTER_INSERT => function($event){
                 $f = @fsockopen('unix:///run/otohub/feed.sock');
                 if ($f) {
-                    fwrite($f, json_encode(['post_id' => $event->sender->id, 'user_id' => Yii::$app->user->getId()]));
+                    fwrite($f, json_encode(['post_id' => $event->sender->id, 'user_id' => \Yii::$app->user->getId()]));
                     fclose($f);
                 }
                 else{
